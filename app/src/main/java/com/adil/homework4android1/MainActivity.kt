626efe1ae6bcc7c73007702dc7508539497b7ec5
Adilhon06 <adubaichik2006@gmail.com>
2024-03-22 21:49:56 +0600

    changed

--- app/src/main/java/com/adil/homework4android1/MainActivity.kt
+++ app/src/main/java/com/adil/homework4android1/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.adil.homework4android1
 
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -8,29 +7,29 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var image: ImageView
-    private lateinit var text: TextView
     private lateinit var editText: EditText
     private lateinit var buttonToWhatsap: MaterialButton
-    private lateinit var buttonToGallery: MaterialButton
+
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            image.setImageURI(uri)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text = this.findViewById(R.id.tv_hint)
         buttonToWhatsap = this.findViewById(R.id.btn_go)
         image = this.findViewById(R.id.iv_whatsapp_icon)
         editText = this.findViewById(R.id.et_space_for_phone_number)
 
         goToWhatsapp()
-
     }
 
     private fun goToWhatsapp() {
@@ -40,6 +39,12 @@ class MainActivity : AppCompatActivity() {
             intent.data = Uri.parse("https://api.whatsapp.com/send?phone=$phoneNumber")
             startActivity(intent)
         }
+        image.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private fun openGallery() {
+        getContent.launch("image/*")
     }
 }
-
